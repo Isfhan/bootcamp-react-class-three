2a5f8441142717255067d4d0e6bc85081015ecb0
Isfhan Ahmed <isfhan729@gmail.com>
2020-06-10 16:24:35 +0500

    Add news data and css

--- src/components/Content.js
+++ src/components/Content.js
@@ -15,6 +15,31 @@ function Content(props) {
                   eleniti placeat, sapiente ipsa iusto omnis fugiat architecto
                  odit debitis ea dignissimos ab voluptas est voluptates possimus pariatur.
             </div>
+
+            <div className="news">
+                <h3>Entertainment News</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea
+                 quos fuga magnam neque earum quibusdam enim
+                vitae, accusamus, eveniet, in cumque. Nihil illo iste velit atque,
+                 libero similique ipsam. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Est ratione eveniet ullam autem et neque consequatur fuga aliquid adipisci magna
+                  m amet quos numquam culpa commodi, vitae, soluta doloribus ea nostrum? Error ad d
+                  eleniti placeat, sapiente ipsa iusto omnis fugiat architecto
+                 odit debitis ea dignissimos ab voluptas est voluptates possimus pariatur.
+            </div>
+
+            <div className="news">
+                <h3>Sports News</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea
+                 quos fuga magnam neque earum quibusdam enim
+                vitae, accusamus, eveniet, in cumque. Nihil illo iste velit atque,
+                 libero similique ipsam. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Est ratione eveniet ullam autem et neque consequatur fuga aliquid adipisci magna
+                  m amet quos numquam culpa commodi, vitae, soluta doloribus ea nostrum? Error ad d
+                  eleniti placeat, sapiente ipsa iusto omnis fugiat architecto
+                 odit debitis ea dignissimos ab voluptas est voluptates possimus pariatur.
+            </div>
+
         </div>
     )
 }
